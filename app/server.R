6d7f8334696c16c95aa7393ee49f037767bf5139
Mathew Hall <mathew.james.hall+gh@gmail.com>
2014-11-30 20:23:02 +0000

    Support for multiple sensors

--- app/server.R
+++ app/server.R
@@ -19,16 +19,20 @@ data <- data %>%
 
 data$Date <- as.POSIXct(strptime(data$Date, format="%F %T"))
 
-
-
+data$Humidity[data$Humidity < 0] <- NA
+data$Temperature[data$Temperature < 0] <- NA
 
 theme_set(theme_minimal())
 
 shinyServer(function(input, output){
 	
-	wide <- reactive({data$SMA.Temperature <- rollmean(data$Temperature,input$sma_window,fill="expand")
-data$SMA.Humidity <- rollmean(data$Humidity,input$sma_window,fill="expand")
-data})
+	wide <- reactive({
+		data %>%
+		group_by(Sensor) %>%
+		mutate(
+			SMA.Temperature = rollmean(Temperature,input$sma_window,fill="expand"),
+			SMA.Humidity       = rollmean(Humidity,input$sma_window,fill="expand") 
+			)})
 	
 long <- reactive({melt(wide(), measure.vars=c("Temperature", "Humidity", "SMA.Temperature","SMA.Humidity"))})
   
@@ -37,7 +41,7 @@ long <- reactive({melt(wide(), measure.vars=c("Temperature", "Humidity", "SMA.Te
     if(input$sma){
       variables <- c("SMA.Temperature","SMA.Humidity")
     }
-  plot <- long() %>% filter(variable %in% variables)%>% ggplot(aes(Date, value, colour=variable)) 
+  plot <- long() %>% filter(variable %in% variables)%>% ggplot(aes(Date, value, colour=Sensor)) 
 		if(input$raw){
 			plot <- plot + geom_line()
 		}
@@ -47,9 +51,9 @@ long <- reactive({melt(wide(), measure.vars=c("Temperature", "Humidity", "SMA.Te
 		if(input$rug){
 			plot <- plot + geom_rug(sides="b", alpha=0.2, colour="black",size=0.1)
 		}
-		plot <- plot + scale_x_datetime(breaks=date_breaks("6 hour"),labels=date_format("%d %b %H:%M"))  + theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) + xlab("\n\nTime")
+		plot <- plot + scale_x_datetime(breaks=date_breaks("4 hour"), minor_breaks=date_breaks("1 hour"), labels=date_format("%d %b %H:%M"))  + theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) + xlab("\n\nTime") + facet_grid(variable ~ .)
 		
-		#, minor_breaks=date_breaks("1 hour"))
+		#
 		plot
   })
   
